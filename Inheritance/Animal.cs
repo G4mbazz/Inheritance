16c0dbd57e6119497c420f99569136dbd01b5d49
G4mbazz <G4mbazz@hotmail.com>
2022-10-31 10:13:52 +0100

    Add project files.

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    internal class Animal
+    {
+        private readonly string Name;
+        private readonly int Age;
+        private readonly string Gender;
+        private readonly bool Legs;
+        private readonly int Weight;
+        
+        public Animal(string _name, int _age, string _gender, bool _legs, int _weight)
+        {
+            Legs = _legs;
+            Name = _name;
+            Age = _age;
+            Gender = _gender;
+            Weight = _weight;
+        }
+        public void AnimalInfo()
+        {
+            Console.WriteLine($"Namn: {Name}\nÅlder: {Age}\nKön: {Gender}\nVikt: {Weight}");
+            if (Legs) Console.WriteLine("Djuret har ben");
+            else Console.WriteLine("Djuret har inga ben");
+        }
+        public void Run()
+        {
+            if (!Legs)
+            {
+                Console.WriteLine("Ditt djur har inga ben och kan därför inte gå eller springa");
+            }
+            else
+            {
+                Console.WriteLine("Ditt djur kan springa!");
+            }
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal test = new Animal("jeff", 12, "Yes please", false, 69);
+            test.AnimalInfo();
+            test.Run();
+        }
+    }
+}
